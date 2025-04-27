0f3de85bc1c05eb366e345d643a31e4b67bfefa9
Yerko Cortés Baeza <ykacorb@gmail.com>
2025-04-27 19:31:50 -0400

    Configuración inicial de plugins sin personalizaciones

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Configuración básica de nvim-tree
+require("nvim-tree").setup({
+  view = {
+    width = 30,  -- ancho de la ventana
+  },
+  filters = {
+    dotfiles = false,  -- mostrar archivos ocultos
+  },
+})
---
+++ lua/plugins.lua
@@ -0,0 +1,66 @@
+return {
+    -- Gestión de plugins
+    "folke/lazy.nvim",
+
+    -- LSP y Desarrollo
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    "folke/neodev.nvim",
+    "tamago324/nlsp-settings.nvim",
+    "jose-elias-alvarez/null-ls.nvim",
+    "b0o/schemastore.nvim",
+    "mfussenegger/nvim-dap",
+    "rcarriga/nvim-dap-ui",
+
+    -- Autocompletado y Snippets
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
+
+    -- Interfaz y UX
+    "goolord/alpha-nvim",
+    "akinsho/bufferline.nvim",
+    "nvim-lualine/lualine.nvim",
+    "SmiteshP/nvim-navic",
+    "nvim-tree/nvim-web-devicons",
+    "lukas-reineke/indent-blankline.nvim",
+    "folke/which-key.nvim",
+    "RRethy/vim-illuminate",
+
+    -- Temas
+    "navarasu/onedark.nvim",
+    "folke/tokyonight.nvim",
+    "lunarvim/colorschemes",
+    "jaredgorski/Mies.vim",
+
+    -- Productividad
+    "numToStr/Comment.nvim",
+    "windwp/nvim-autopairs",
+    "nvim-treesitter/nvim-treesitter",
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    "nvim-telescope/telescope.nvim",
+    "nvim-telescope/telescope-fzf-native.nvim",
+    "akinsho/toggleterm.nvim",
+    "ahmedkhalf/project.nvim",
+    -- "tamago324/lir.nvim",
+    "kyazdani42/nvim-tree.lua",
+
+    -- Rendimiento
+    "LunarVim/bigfile.nvim",
+
+    -- Git
+    "lewis6991/gitsigns.nvim",
+
+    -- Utilidades
+    "nvim-lua/plenary.nvim",
+    "Tastyep/structlog.nvim",
+
+  "folke/neodev.nvim",
+  "folke/which-key.nvim",
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+}
