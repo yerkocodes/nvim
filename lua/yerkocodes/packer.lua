7595f4a565640f0e4f4829733edbe112921736b1
Yerko Cortés Baeza <ykacorb@gmail.com>
2025-05-01 21:43:33 -0400

    Configura telescope para búsqueda de archivos y también tema monocolor

--- lua/yerkocodes/packer.lua
+++ lua/yerkocodes/packer.lua
@@ -7,4 +7,16 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- Finder files
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- Colorscheme
+  use {'kdheepak/monochrome.nvim', config = function()
+	  vim.cmd 'colorscheme monochrome'
+  end}
+
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,14 +74,34 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  ["monochrome.nvim"] = {
+    config = { "\27LJ\2\n:\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\27colorscheme monochrome\bcmd\bvim\0" },
+    loaded = true,
+    path = "/home/ykacorb/.local/share/nvim/site/pack/packer/start/monochrome.nvim",
+    url = "https://github.com/kdheepak/monochrome.nvim"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/home/ykacorb/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
+  },
+  ["plenary.nvim"] = {
+    loaded = true,
+    path = "/home/ykacorb/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    url = "https://github.com/nvim-lua/plenary.nvim"
+  },
+  ["telescope.nvim"] = {
+    loaded = true,
+    path = "/home/ykacorb/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    url = "https://github.com/nvim-telescope/telescope.nvim"
   }
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: monochrome.nvim
+time([[Config for monochrome.nvim]], true)
+try_loadstring("\27LJ\2\n:\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\27colorscheme monochrome\bcmd\bvim\0", "config", "monochrome.nvim")
+time([[Config for monochrome.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
